77f9931aa11cddcebd1ec3af8dc469ea3cf52f19
Plotvochka <106927858+Plotvochka@users.noreply.github.com>
2024-08-24 14:55:51 +0300

    full done

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -4,9 +4,15 @@ export default function Options({ onChange, reset, total }) {
   const shouldShowReset = total >= 1;
   return (
     <div>
-      <button onClick={() => onChange("good")}>Good</button>
-      <button onClick={() => onChange("neutral")}>Neutral</button>
-      <button onClick={() => onChange("bad")}>Bad</button>
+      <button className={css.btn} onClick={() => onChange("good")}>
+        Good
+      </button>
+      <button className={css.btn} onClick={() => onChange("neutral")}>
+        Neutral
+      </button>
+      <button className={css.btn} onClick={() => onChange("bad")}>
+        Bad
+      </button>
       {shouldShowReset && <button onClick={reset}>Reset</button>}
     </div>
   );
